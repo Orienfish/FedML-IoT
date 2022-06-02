a173211e613cd3ed76e2ef718bc0ea1d820f757c
Orienfish <x1yu@ucsd.edu>
2022-06-02 16:11:48 -0700

    fix bug

--- fedcnn_rpi_client.py
+++ fedcnn_rpi_client.py
@@ -118,7 +118,7 @@ def load_data(args, dataset_name):
         print(
             "============================Starting loading {}==========================#".format(
                 args.dataset))
-        data_dir = './../../../data/' + args.dataset
+        data_dir = './../data/' + args.dataset
         train_data_num, test_data_num, train_data_global, test_data_global, \
         train_data_local_num_dict, train_data_local_dict, test_data_local_dict, \
         class_num = data_loader(args.dataset, data_dir, args.partition_method,
@@ -133,7 +133,7 @@ def load_data(args, dataset_name):
         print(
             "============================Starting loading {}==========================#".format(
                 args.dataset))
-        data_dir = './../../../data/' + args.dataset
+        data_dir = './../data/' + args.dataset
         train_data_num, test_data_num, train_data_global, test_data_global, \
         train_data_local_num_dict, train_data_local_dict, test_data_local_dict, \
         class_num = data_loader(args.dataset, data_dir, args.partition_method,
