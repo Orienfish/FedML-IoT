6daff29da188428d7519ea743213ae3fa613c6c6
Orienfish <x1yu@ucsd.edu>
2022-06-03 22:51:38 -0700

    fix scirpt

--- run_client.sh
+++ run_client.sh
@@ -4,6 +4,7 @@
 # The script will run continuously run clients, with one minute interval between two trials
 
 while true
+do
   python3 fedcnn_rpi_client.py --server_ip http://132.239.17.133:5000 --client_uuid "$1"
   sleep 60s
 done
