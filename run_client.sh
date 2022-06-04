79ff36a1c88db3652d9c724113f5a475e22eca29
Orienfish <x1yu@ucsd.edu>
2022-06-03 22:11:54 -0700

    update continuously running client script

--- run_client.sh
+++ run_client.sh
@@ -1,1 +1,9 @@
-python3 fedcnn_rpi_client.py --server_ip http://132.239.17.133:5000 --client_uuid "$1"
+# Usage:
+#   Run FedML Client with $uuid
+#     bash ./run_client.sh $uuid
+# The script will run continuously run clients, with one minute interval between two trials
+
+while true
+  python3 fedcnn_rpi_client.py --server_ip http://132.239.17.133:5000 --client_uuid "$1"
+  sleep 60s
+done
