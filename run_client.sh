6ed80e980f125020b8710fba41751efccfe69ec6
Orienfish <x1yu@ucsd.edu>
2022-06-05 09:51:11 -0700

    fix script

--- run_client.sh
+++ run_client.sh
@@ -5,6 +5,6 @@
 
 while true
 do
-  python3 fedcnn_rpi_client.py --server_ip http://132.239.17.133:5000 --client_uuid "$1" 2>&1 | tee output.log
+  python3 fedcnn_rpi_client.py --server_ip http://132.239.17.132:5000 --client_uuid "$1" 2>&1 | tee output.log
   sleep 60s
 done
