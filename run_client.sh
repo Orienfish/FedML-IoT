ddd0b919be269b07a7eca8f7b30fda4305db71f0
Orienfish <x1yu@ucsd.edu>
2022-06-15 18:26:03 -0700

    update script

--- run_client.sh
+++ run_client.sh
@@ -3,8 +3,17 @@
 #     bash ./run_client.sh $uuid
 # The script will run continuously run clients, with one minute interval between two trials
 
+if [ $# -eq 0 ]
+  then
+    offset=3
+    client_id=$(($offset+3))
+else
+  client_id=$1
+fi
+echo $client_id
+
 while true
 do
-  python3 fedcnn_rpi_client.py --server_ip http://132.239.17.132:5000 --client_uuid "$1" 2>&1 | tee output.log
+  python3 fedcnn_rpi_client.py --server_ip http://132.239.17.132:5000 --client_uuid $client_id 2>&1 | tee output.log
   sleep 60s
 done
